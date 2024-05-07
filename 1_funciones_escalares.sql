75a3855ac6684dfd105dfa885518dfb643f5a1f4
MayePalomino <mayepalomino2020@gmail.com>
2024-05-07 10:53:49 -0400

    adding cast/convert

--- 1_funciones_escalares.sql
+++ 1_funciones_escalares.sql
@@ -1,40 +1,60 @@
 use Adventureworks2022;
--- I funciones de fecha
+
 /*************************************************************************************
 Contexto:	Funciones escalares en SQL Server son aquellas que toman uno o más argumentos 
 			y devuelven un único valor basado en esos argumentos
 			, sin afectar el estado de la base de datos.
 **************************************************************************************/
 
+
+/*************************************************************************************
+-- I Funciones de fecha
+**************************************************************************************/
+
 -- 1. Calcular el tiempo de duración de venta de un producto, en días
-SELECT DATEDIFF(DAY, SellStartDate, SellEndDate) AS DaysInMarket 
+SELECT name,SellStartDate,SellEndDate, DATEDIFF(DAY, SellStartDate, SellEndDate) AS DaysInMarket 
 FROM Production.Product WHERE SellEndDate IS NOT NULL;
 
 -- 2. Productos que actualmente se encuetran en venta
-SELECT ProductID FROM Sales.Product WHERE SellEndDate IS NULL;
+SELECT name,SellStartDate,SellEndDate, DATEDIFF(YEAR, SellStartDate, SellEndDate) AS YearsInMarket 
+FROM Production.Product WHERE SellEndDate IS NOT NULL;
 
--- 3. Productos que han estado en venta por más de un año
-SELECT ProductID FROM Sales.Product 
-WHERE DATEDIFF(YEAR, SellStartDate, GETDATE()) >= 1 AND SellEndDate IS NOT NULL;
+-- 3. Qué fecha resulta si agregamos 10 dias, a una fecha
+SELECT name,SellStartDate, DATEADD(DAY, 10, SellStartDate) AS Dia_llegada 
+FROM Production.Product WHERE SellEndDate IS NOT NULL;
 
---  II funciones numericas
--- 1. Cálculo del precio promedio de un producto en todas las órdenes
-SELECT p.name, AVG(OrderQty * UnitPrice) AS AvgPricePerOrder 
+-- 4. Qué fecha resulta si agregamos 10 dias a la fecha de hoy
+SELECT getdate() as toda, dateadd(day, 10 ,getdate());
+
+
+
+/*************************************************************************************
+-- II Funciones numéricas
+**************************************************************************************/
+
+-- 1. Cálculo del precio promedio de todos producto en las ordenes del año 2011
+SELECT AVG(ListPrice) AS AvgQtyOrdered
 FROM Sales.SalesOrderDetail  s
+inner join Sales.SalesOrderHeader h
+on s.SalesOrderID = h.SalesOrderID
 inner join Production.Product p
 on s.ProductID = p.productID
-GROUP BY p.name order by p.Name;
+WHERE YEAR(h.OrderDate) =2011;
+-- 
 
--- 2. Producto más vendido en términos de cantidad
-SELECT p.Name, SUM(OrderQty) AS TotalQuantity 
+-- 2. Suma de cantidad vendida de productos cuyo nombre contengan la palabra 'bike'
+select sum(OrderQty)
+-- select p.Name, OrderQty
 FROM Sales.SalesOrderDetail s
 inner join Production.Product p
 on s.ProductID = p.productID
-GROUP BY p.name ORDER BY TotalQuantity;
+WHERE p.name like '%bike%'
 
 
 
---  III funciones de cadena
+/*************************************************************************************
+-- III funciones de cadena
+**************************************************************************************/
 
 --  IV Regular expressions
 -- 1. Second character is 'r'
@@ -52,6 +72,14 @@ SELECT ProductID, name,
 				END AS FirstName
 FROM Production.Product;
 
+/*************************************************************************************
+-- IV Conversión de datos
+**************************************************************************************/
+--  CAST: Utilizada para conversiones sin formato especifico, formato standar ANSI
+SELECT CAST('2022-05-15' AS varchar) AS ConvertedDate;
+--  CONVERT: Permite convertir y a la vez, formatear el argumento
+
+SELECT CONVERT(VARCHAR, GETDATE(), 126) AS FormattedDate;
 
 
 
